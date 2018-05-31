18a369c17a7bea3d910307d4638efcb4fc13d6d9
Unknown <ishnn032n@gmail.com>
2018-05-31 23:50:56 +0900

    Add Jumper(+Script control) / Drop, Spinner Platform

---
+++ Assets/Resources/Scripts/Platforms/Jumper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rigi = other.GetComponent<Rigidbody>();
+        rigi.velocity = Vector3.zero;
+        rigi.AddForce(this.transform.right * 2f, ForceMode.Impulse);
+    }
+}
